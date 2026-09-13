c73f626c40c4d8429987cb8c416daed1a8f89963
Jenylee Diaz Munoz <jdiazmu1@binghamton.edu>
2026-09-13 10:55:29 -0400

    cleaned up code and added better error messages

--- submit/prj1-sol/bits571.py
+++ submit/prj1-sol/bits571.py
@@ -2,14 +2,17 @@ import json
 import re
 import sys
 
-# lexer
+'''
+Lexer is the scanner/program that transfers a character
+stream into a token-stream and removes comments and such
+'''
 def is_hex_digit(ch):
     return ch.isdigit() or ch in "abcedfABCDEF"
 
 def valid_integer(text):
-    # check if text is a valid integer
+    # check if text is a valid integer 'xX'
     if text.startswith(("0x", "0X")):
-        body = text[2:]
+        body = text[2:] #0x[2][3]...
 
         if not body:
             return False
@@ -35,37 +38,33 @@ def lex(source):
     # convert input string into a list of tokens
     tokens = []
     i = 0
-
+    
     while i < len(source):
         ch = source[i]
-
         # ignore whitespace
         if ch.isspace():
             i += 1
             continue
+        # get rid of comments as noted by project description
         if source.startswith("//", i):
             i += 2
-
             while i < len(source) and source[i] != "\n":
                 i += 1
-
             continue
-        # two character operators
+        # two character operators --> move index by two
         if source.startswith("<<", i):
             tokens.append(("<<", "<<"))
             i += 2
-            continue
-        
+            continue 
         if source.startswith(">>", i):
             tokens.append((">>", ">>"))
             i += 2
             continue
-
+        # only move one if it is one character operand
         if ch in "~^&|()":
             tokens.append((ch, ch))
             i+= 1
             continue
-        
         # integers 0 - 9
         if ch.isdigit():
             start = i
@@ -74,7 +73,6 @@ def lex(source):
                 i +=2
                 while i < len(source):
                     current = source[i]
-
                     if is_hex_digit(current) or current == "_":
                         i +=1
                     else:
@@ -84,21 +82,23 @@ def lex(source):
                 i += 1
                 while i < len(source):
                     current = source[i]
-
                     if current.isdigit() or current == "_":
                         i += 1
                     else:
                         break
             text = source[start:i]
-
             if not valid_integer(text):
                 raise SyntaxError("invalid integer: " + text)
             tokens.append(("INTEGER", text))
             continue
         # anything else that is invalid
-        raise SyntaxError("invalid character : " + repr(ch))
+        raise SyntaxError("invalid character : " + repr(ch) + " \nExpected an integer, operator, (~, ^, &, |, <<, >>), or a parenthesis")
     return tokens
 
+'''
+    Parser is a program that given a token stream and a CFG,
+    produces a prase tree/AST
+'''
 # parser works with tokens and position
 tokens = [] # set it empty to initialize
 pos = 0
@@ -113,7 +113,6 @@ def peek():
 def peek_type():
     # type of the current token
     token = peek()
-
     if token is None:
         return None
     else:
@@ -125,10 +124,8 @@ def consume(expected_type):
     # consume and return of expected token
     global pos
     token = peek()
-
     if token is None:
         raise SyntaxError("expected " + expected_type + ", found end of input ")
-    
     if token[0] != expected_type:
         raise SyntaxError(
             "expected " + expected_type + ", found " + token[0]
@@ -145,7 +142,7 @@ def parse_program():
     result = []
     while pos < len(tokens):
         if not starts_expression(peek_type()):
-            raise SyntaxError("unexpected token: " + str(peek()[1]))
+            raise SyntaxError("expecting 'EOF' but got " + str(peek()[1]))
         result.append(parse_expr())
     return result
 
@@ -169,12 +166,19 @@ def parse_shift_expr():
             "operand1": left,
             "operand2": right
         }
+        if peek_type() in ("<<", ">>"):
+            raise SyntaxError("shift operators are non-associative: " + peek()[1] + " cannot follow another shift expression without parentheses" )
     return left
 
 # : xorExpr  ( ( '&' | '|' ) xorExpr)*
 def parse_bitwise_expr():
     left = parse_xor_expr()
-    if peek_type() in ("&" ,"|"):
+    '''
+    using 'if' statement here led to an 
+    error if it read one of the two symbols it would fail
+    to read the one after the first
+    '''
+    while peek_type() in ("&" ,"|"):
         operator = peek_type()
         consume(operator)
         right = parse_xor_expr()
@@ -192,6 +196,7 @@ def parse_xor_expr():
     left = parse_unary_expr()
     if peek_type() == ("^"):
         consume("^")
+        # call xor once we find the correct character '^'
         right = parse_xor_expr()
 
         return {
@@ -219,7 +224,6 @@ def parse_unary_expr():
 # | '(' expr ');
 def parse_primary_expr():
     token_type = peek_type()
-
     if token_type == "INTEGER":
         token = consume("INTEGER")
         text = token[1]
@@ -228,9 +232,9 @@ def parse_primary_expr():
         cleaned = text.replace("_", "")
 
         if cleaned.startswith(("0x", "0X")):
-            return int(cleaned, 16)
+            return int(cleaned, 16) #base 16 number
 
-        return int(cleaned, 10)
+        return int(cleaned, 10) #base 10 number 
     if token_type == "(":
         consume("(")
 
@@ -240,8 +244,8 @@ def parse_primary_expr():
         return result
 
     if peek() is None:
-        raise SyntaxError("Expected expression found at end of input")
-    raise SyntaxError("expected expression, found " + str(peek()[1]))
+        raise SyntaxError("Expected expression, but we reached the end of the input")
+    raise SyntaxError("expected expression, found " + str(peek()[1]) + " instead")
 
 def main():
     global tokens
--- submit/prj1-sol/make.sh
+++ submit/prj1-sol/make.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# TODO: use this script to compile any artifacts needed by your parser,
-# assuming that the current directory is the one containing this file.
-
 set -e
 
 DIR=$(dirname "$0")
